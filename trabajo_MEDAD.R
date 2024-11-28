1e538864fddd21e04a1becdb4ae8367f6f5d1627
Alvarolpe <158449001+Alvarolpe@users.noreply.github.com>
2024-11-28 10:03:11 +0100

    PCA / MDS

--- trabajo_MEDAD.R
+++ trabajo_MEDAD.R
@@ -69,6 +69,25 @@ result <- vec12 %*% sqrt(lambda_12); result
 
 d.names <- row.names(Spain)
 
+  ## 8: Representación grafica 
 plot(result, pch=19, main="Resultado del MDS clásico", xlab="x",ylab="y",
      xlim=c(-10000,10000),ylim=c(-80,50))
 text(result-c(5,7), labels=d.names,cex=0.9, font=2)
+
+  ## 11: MDS con variables
+# dist.var <- as.matrix(cor(Spain))
+dist.var <- as.matrix(prcomp(scale(Spain, center = TRUE, scale = TRUE))$rotation)
+
+d.names <- colnames(Spain)
+
+n <- dim(dist.var)[1]
+
+fit <- cmdscale(dist.var, eig = TRUE, k = 2); fit
+
+x <- fit$points[,1]
+y <- fit$points[,2]
+
+# Representación grafica
+plot(x,y, pch=19, main="Resultado del MDS sobre las Variables", xlab="x",ylab="y",
+     xlim=c(-0.5,0.5),ylim=c(-0.5,0.5))
+text(result-c(0.1,0.1), labels=d.names, font=2)
