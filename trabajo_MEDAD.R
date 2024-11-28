32eb17bc3eabd6a006470617d947c07dc0cbd091
Alvarolpe <158449001+Alvarolpe@users.noreply.github.com>
2024-11-28 09:21:08 +0100

    PCA / MDS (manual)

--- trabajo_MEDAD.R
+++ trabajo_MEDAD.R
@@ -38,3 +38,36 @@ datos.scale.pca.varcov$rotation
 
 # Representación grafica
 ggbiplot(datos.scale.pca.varcov, labels = row.names(datos))
+
+
+
+# MDS en R (manual)
+
+library(philentropy)
+dist.dat <- as.matrix(dist(Spain))
+
+n <- dim(dist.dat)[1]
+
+A <- (-1/2)*dist.dat^2 # Matriz A
+
+# Matriz de centrado
+H <- diag(rep((n-1)/n,length.out=n))  # En la diagonal 6/7
+H[which(H==0)] <- -1/n   # En las otras posiciones, -1/7
+H
+
+# Matriz B = HAH
+B <- H %*% A %*% H; B
+
+lambda <- eigen(B)$values # autovalores de B
+autovec <- eigen(B)$vectors #autovectores de B
+
+vec12 <- autovec[,1:2]
+lambda_12 <- diag(lambda[1:2])
+
+result <- vec12 %*% sqrt(lambda_12); result
+
+d.names <- row.names(Spain)
+
+plot(result, pch=19, main="Resultado del MDS clásico", xlab="x",ylab="y",
+     xlim=c(-10000,10000),ylim=c(-80,50))
+text(result-c(5,7), labels=d.names,cex=0.9, font=2)
